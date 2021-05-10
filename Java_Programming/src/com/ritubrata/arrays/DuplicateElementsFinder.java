6b804ae3279fd6b20ce34e30280b9f6db27a7f05
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-05-10 17:11:55 +0530

    initial commit for Java programming

---
+++ Java_Programming/src/com/ritubrata/arrays/DuplicateElementsFinder.java
@@ -0,0 +1,31 @@
+package com.ritubrata.arrays;
+
+import java.util.HashSet;
+
+public class DuplicateElementsFinder {
+	public static void main(final String[] args){
+		final String[] strArray = {"Java", "JSP", "Servlets", "Java", "Struts", "JSP", "JDBC"};
+		byForLoops(strArray);
+		byHashSet(strArray);
+	}
+
+	private static void byForLoops(final String[] strArray) {
+		for (int i = 0; i < strArray.length-1; i++){
+			for (int j = i+1; j < strArray.length; j++){
+				if( strArray[i].equals(strArray[j]) && i != j ){
+					System.out.println("Duplicate Element is : "+strArray[j]);
+				}
+			}
+		}
+	}
+
+	private static void byHashSet(final String[] strArray) {
+		final HashSet<String> set = new HashSet<String>();
+		for (final String arrayElement : strArray){
+			if(!set.add(arrayElement)){
+				System.out.println("Duplicate Element is : "+arrayElement);
+			}
+		}
+	}
+
+}
