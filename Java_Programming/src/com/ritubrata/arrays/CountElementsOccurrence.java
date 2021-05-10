83a7aebdf2d4556ef07c38892a2e7ac126f2618a
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-05-10 17:34:24 +0530

    Count occurrence of elements of an array

---
+++ Java_Programming/src/com/ritubrata/arrays/CountElementsOccurrence.java
@@ -0,0 +1,26 @@
+package com.ritubrata.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class CountElementsOccurrence {
+	static void arrayElementCount(final int inputArray[]){
+		final HashMap<Integer, Integer> elementCountMap = new HashMap<Integer, Integer>();
+		for (final int i : inputArray){
+			if(elementCountMap.containsKey(i)){
+				elementCountMap.put(i, elementCountMap.get(i)+1);
+			}else {
+				elementCountMap.put(i, 1);
+			}
+		}
+		System.out.println("Input Array : "+Arrays.toString(inputArray));
+		System.out.println("Element Count : "+elementCountMap);
+		System.out.println("=======================================");
+	}
+
+	public static void main(final String[] args){
+		arrayElementCount(new int[]{4, 5, 4, 5, 4, 6});
+		arrayElementCount(new int[]{12, 9, 12, 9, 10, 9, 10, 11});
+		arrayElementCount(new int[]{891, 187, 891, 187, 891, 476, 555, 741});
+	}
+}
