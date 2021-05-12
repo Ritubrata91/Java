08114aaf852321943a2d0f3b7e41ffc4e504e2ed
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-05-12 23:32:11 +0530

    features of finally block

---
+++ Java_Programming/src/com/ritubrata/exceptions/FinallyBlockProperties.java
@@ -0,0 +1,38 @@
+package com.ritubrata.exceptions;
+
+public class FinallyBlockProperties {
+
+	public static void main(final String[] args) {
+		printFromCatchAndFinally();
+		System.out.println("********************************");
+		System.out.println(returnFromCatchAndFinally());
+	}
+
+	private static void printFromCatchAndFinally() {
+		int i ;
+		try {
+			i =2/0;
+		}catch (final ArithmeticException e) {
+			i =3;
+			System.out.println(i);
+		}finally {
+			i =4;
+			System.out.println(i);
+		}
+	}
+
+	private static int returnFromCatchAndFinally() {
+		int i ;
+		try {
+			i =2/0;
+		}catch (final ArithmeticException e) {
+			i =3;
+			return i;
+		}finally {
+			i =4;
+			return i;
+		}
+
+	}
+
+}
