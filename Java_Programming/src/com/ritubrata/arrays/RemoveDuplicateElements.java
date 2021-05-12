e25e91388b0e9ee7dc7cedb5d79856afa4c1fbd0
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-05-12 23:31:57 +0530

    remove duplicates from array

--- Java_Programming/src/com/ritubrata/arrays/RemoveDuplicateElements.java
+++ Java_Programming/src/com/ritubrata/arrays/RemoveDuplicateElements.java
@@ -1,6 +1,7 @@
 package com.ritubrata.arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class RemoveDuplicateElements {
 
@@ -21,9 +22,18 @@ public class RemoveDuplicateElements {
 		System.out.println("==============================");
 	}
 
+	public static void removeDuplicatesByHashSet(final int[] arrayWithDuplicates) {
+		final HashSet<Integer> set = new HashSet<>();
+		for (final Integer arrayElement : arrayWithDuplicates){
+			set.add(arrayElement);
+		}
+		System.out.println("Array Without Duplicates : "+set);
+		System.out.println("==============================");
+	}
+
 	public static void main(final String[] args){
-		removeDuplicates(new int[] {4, 3, 2, 4, 9, 2});
-		removeDuplicates(new int[] {1, 2, 1, 2, 1, 2});
+		removeDuplicatesByHashSet(new int[] {4, 3, 2, 4, 9, 2});
+		removeDuplicatesByHashSet(new int[] {1, 2, 1, 2, 1, 2});
 		removeDuplicates(new int[] {15, 21, 11, 21, 51, 21, 11});
 		removeDuplicates(new int[] {7, 3, 21, 7, 34, 18, 3, 21});
 	}
