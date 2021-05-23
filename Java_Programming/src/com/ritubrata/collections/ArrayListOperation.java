fc9b1df56310ff9e81754b5afdd0cd90cbcccfe9
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-05-23 18:15:37 +0530

    Byte and collection related problems.

---
+++ Java_Programming/src/com/ritubrata/collections/ArrayListOperation.java
@@ -0,0 +1,17 @@
+package com.ritubrata.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListOperation {
+	public static void main(final String[] args) {
+		final List<Integer> list = new ArrayList<>();
+		for(int i =0; i < 100; i++) {
+			list.add(0);
+			list.add(0);
+			list.add(1);
+		}
+		System.out.println(list.size());
+	}
+
+}
