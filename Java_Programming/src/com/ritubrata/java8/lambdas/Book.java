807154a661a22edb875e7cb51c52644db066aa32
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-07-06 20:38:20 +0530

    added few more stream related examples.

--- Java_Programming/src/com/ritubrata/java8/lambdas/Book.java
+++ Java_Programming/src/com/ritubrata/java8/lambdas/Book.java
@@ -4,14 +4,15 @@ public class Book {
 	private String authorFName;
 	private String authorLName;
 	private int pages;
+	private String category;
 
 	public Book(final String title, final String authorFName, final String authorLName,
-			final int pages) {
+			final int pages, final String category) {
 		this.title = title;
 		this.authorFName = authorFName;
 		this.authorLName = authorLName;
 		this.pages = pages;
-
+		this.category = category;
 	}
 
 	public String getTitle() {
@@ -44,6 +45,14 @@ public class Book {
 
 	public void setPages(final int pages) {
 		this.pages = pages;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(final String category) {
+		this.category = category;
 	}
 	@Override
 	public String toString()
--- Java_Programming/src/com/ritubrata/java8/lambdas/Lambdas_Related_To_Stream.java
+++ Java_Programming/src/com/ritubrata/java8/lambdas/Lambdas_Related_To_Stream.java
@@ -1,96 +1,61 @@
 package com.ritubrata.java8.lambdas;
 
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Lambdas_Related_To_Stream {
 
 
-	private static List<Book> useOfSummingInt(final Book... books) {
-		final List<Book> bookList = new ArrayList<>();
-		Collections.addAll(bookList, books);
+	public static void main(final String[] args) {
+		//total pages in your book collection
+		final Book book1 = new Book("Miss Peregrine's Home for Peculiar Children",
+				"Ranson", "Riggs", 382, "Film");
+		final Book book2 = new Book("Harry Potter and The Sorcerers Stone",
+				"JK", "Rowling", 411, "Film");
+		final Book book3 = new Book("The Cat in the Hat",
+				"Dr", "Seuss", 45, "Comedy");
+
+		final List<Book> bookList = Arrays.asList(book1, book2, book3);
+
+		//use of summingInt
 		final int total = bookList.stream()
 				.collect(Collectors.summingInt(Book::getPages));
 		System.out.println(total);
-		return bookList;
-	}
 
-	private static void removeDuplicates(final Book book1, final Book book2, final Book book3) {
-		//create a list with duplicates
-		final List<Book> dupBooks = Arrays.asList(book1, book2, book3, book1, book2);
-		System.out.println("Before removing duplicates: ");
-		System.out.println(dupBooks.toString());
+		//use of count
+		System.out.println("no of books having more than 30 pages : " + bookList.stream()
+		.filter(book -> book.getPages()>30)	.count());
 
-		final Collection<Book> noDups = new HashSet<>(dupBooks);
-		System.out.println("After removing duplicates: ");
-		System.out.println(noDups.toString());
-	}
+		//use of findAny
+		System.out.println(bookList.stream()
+				.filter(e->e.getTitle().contains("The")).findAny());
 
-	public static void main(final String[] args) {
+		//use of groupBy
+		final Map<String, List<Book>> map = bookList.stream()
+				.collect(Collectors.groupingBy(Book::getCategory));
+		map.forEach((category,bookListTemp)->System.out.println("Name: "+category+" ==>"+bookListTemp));
 
-		//total pages in your book collection
-		final Book book1 = new Book("Miss Peregrine's Home for Peculiar Children",
-				"Ranson", "Riggs", 382);
-		final Book book2 = new Book("Harry Potter and The Sorcerers Stone",
-				"JK", "Rowling", 411);
-		final Book book3 = new Book("The Cat in the Hat",
-				"Dr", "Seuss", 45);
-
-		final List<Book> books = useOfSummingInt(book1, book2, book3);
-
-		removeDuplicates(book1, book2, book3);
-
-		final List<String> list = books.stream()
-				.map(Book::getAuthorLName)
-				.collect(Collectors.toList());
-		System.out.println(list);
-
-		//example of using Set to eliminate dups and sort automatically
-		final Set<Integer> numbers = new HashSet<>(asList(4, 3, 3, 3, 2, 1, 1, 1));
-		System.out.println(numbers.toString());
-
-		Arrays.asList("red", "green", "blue")
-		.stream()
-		.sorted()
-		.findFirst()
-		.ifPresent(System.out::println);
+		//use of sorted
+		Arrays.asList("red", "green", "blue").stream().sorted()
+		.findFirst().ifPresent(System.out::println);
 
 		//example of Stream.of with a filter
 		Stream.of("apple", "pear", "banana", "cherry", "apricot")
-		.filter(fruit -> fruit.startsWith("a"))
-		//if the foreach is removed, nothing will print,
-		//the foreach makes it a terminal event
-		.forEach(fruit -> System.out.println("Starts with A: " + fruit));
+		.filter(fruit -> fruit.startsWith("a")).forEach(fruit -> System.out.println("Starts with a: " + fruit));
 
-		//using a stream and map operation to create a list of words in caps
-		final List<String> collected = Stream.of("Java", " Rocks")
-				.map(String::toUpperCase)
-				.collect(toList());
-		System.out.println(collected.toString());
-		IntStream.range(1, 4)
-		.forEach(System.out::println);
+		//use of average
+		Arrays.stream(new int[]{1, 2, 3, 4}).map(n -> n * n)
+		.average().ifPresent(System.out::println);
 
-		//find the average of the numbers squared
-		Arrays.stream(new int[]{1, 2, 3, 4})
-		.map(n -> n * n)
-		.average()
-		.ifPresent(System.out::println);
+		//use of mapToInt and max
+		Stream.of(1.5, 2.3, 3.7).mapToInt(Double::intValue)
+		.max().ifPresent(System.out::println);
 
-		//map doubles to ints
-		Stream.of(1.5, 2.3, 3.7)
-		.mapToInt(Double::intValue)
-		.forEach(System.out::println);
+
+
 	}
 
 
