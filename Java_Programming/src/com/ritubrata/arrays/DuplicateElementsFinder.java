0483572a1d4c56ccf74bbdc3650533e0afbd008e
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-08-02 23:27:47 +0530

    Duplicate element in Array using Stream

--- Java_Programming/src/com/ritubrata/arrays/DuplicateElementsFinder.java
+++ Java_Programming/src/com/ritubrata/arrays/DuplicateElementsFinder.java
@@ -1,22 +1,22 @@
 package com.ritubrata.arrays;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DuplicateElementsFinder {
 	public static void main(final String[] args){
 		final String[] strArray = {"Java", "JSP", "Servlets", "Java", "Struts", "JSP", "JDBC"};
-		byForLoops(strArray);
+		System.out.println(byStream(strArray));
 		byHashSet(strArray);
 	}
 
-	private static void byForLoops(final String[] strArray) {
-		for (int i = 0; i < strArray.length; i++){
-			for (int j = i+1; j < strArray.length; j++){
-				if( strArray[i].equals(strArray[j]) && i != j ){
-					System.out.println("Duplicate Element is : "+strArray[j]);
-				}
-			}
-		}
+	private static Set<String> byStream(final String[] strArray) {
+		final HashSet<String> set = new HashSet<String>();
+		return Arrays.stream(strArray)
+				.filter(s->!set.add(s))
+				.collect(Collectors.toSet());
 	}
 
 	private static void byHashSet(final String[] strArray) {
