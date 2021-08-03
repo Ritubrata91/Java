f55e602418987c2b28d2878181fd845eae4966e1
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-08-03 22:05:29 +0530

    Array Element Count using Stream.

--- Java_Programming/src/com/ritubrata/arrays/ArrayElementCount.java
+++ Java_Programming/src/com/ritubrata/arrays/ArrayElementCount.java
@@ -2,9 +2,13 @@ package com.ritubrata.arrays;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class CountOccurrenceOfElements {
-	static void arrayElementCount(final int inputArray[]){
+public class ArrayElementCount {
+	static void byUsingMap(final int inputArray[]){
 		final HashMap<Integer, Integer> elementCountMap = new HashMap<Integer, Integer>();
 		if(inputArray.length == 0) {
 			System.out.println("Array Is Empty");
@@ -22,10 +26,21 @@ public class CountOccurrenceOfElements {
 		}
 	}
 
+	static void byUsingStreeam(){
+		final List<String> list = Arrays.asList("B", "A", "A", "C", "B", "A");
+
+		final Map<String, Long> frequencyMap =
+				list.stream().collect(Collectors.groupingBy(Function.identity(),
+						Collectors.counting()));
+
+		for (final Map.Entry<String, Long> entry: frequencyMap.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+	}
+
 	public static void main(final String[] args){
-		arrayElementCount(new int[]{4, 5, 4, 5, 4, 6});
-		arrayElementCount(new int[]{12, 9, 12, 9, 10, 9, 10, 11});
-		arrayElementCount(new int[]{891, 187, 891, 187, 891, 476, 555, 741});
-		arrayElementCount(new int[]{});
+		byUsingMap(new int[]{4, 5, 4, 5, 4, 6});
+		byUsingMap(new int[]{});
+		byUsingStreeam();
 	}
 }
