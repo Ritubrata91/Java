3d101f1d3c5c378e356b81eb8b39a4759835df1a
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-09-09 19:29:54 +0530

    Use of FlatMap in 2D array.

--- Java_Programming/src/com/ritubrata/java8/UsageOfFlatMap.java
+++ Java_Programming/src/com/ritubrata/java8/UsageOfFlatMap.java
@@ -3,6 +3,7 @@ package com.ritubrata.java8;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class UsageOfFlatMap {
@@ -17,5 +18,16 @@ public class UsageOfFlatMap {
 				.stream()
 				.flatMap(List::stream)
 				.collect(Collectors.toList()));
+
+		final int[][] arr = new int[3][2];
+		arr[0][0] = 1;
+		arr[0][1] = 2;
+		arr[1][0] = 3;
+		arr[1][1] = 4;
+		arr[2][0] = 5;
+		arr[2][1] = 6;
+
+		Stream.of(arr).flatMapToInt(IntStream::of).forEach(System.out::println);
+
 	}
 }
