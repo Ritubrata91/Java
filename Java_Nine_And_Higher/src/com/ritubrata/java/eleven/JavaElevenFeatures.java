317b6f9facfcfd7dbb4215b5d1f48b6949bd5c8d
Ritubrata Chatterjee <ritubrata91@gmail.com>
2022-06-04 22:45:06 +0530

    Java 11: Predicate.not() method.

--- Java_Nine_And_Higher/src/com/ritubrata/java/eleven/JavaElevenFeatures.java
+++ Java_Nine_And_Higher/src/com/ritubrata/java/eleven/JavaElevenFeatures.java
@@ -4,17 +4,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class JavaElevenFeatures {
 
 	public static void main(final String[] args) throws IOException {
+		//fileReadWrite();
+		predicateNotMethod();
+	}
+
+	private static void fileReadWrite() throws IOException {
 		final Path path = Paths.get("./resources/sample.txt");
 		final String fileContent = Files.readString(path);
 		System.out.println(fileContent);
 
-
 		final Path new_path = Paths.get("./resources/sample_two.txt");
 		Files.writeString(new_path, fileContent.concat("jkl"));
+	}
+
+	private static void predicateNotMethod() throws IOException {
+		final List<Integer> list = List.of(2,3,4,56,45,76,97);
+		list.stream().filter(Predicate.not(n->n%2==0)).forEach(System.out::println);
 	}
 
 }
