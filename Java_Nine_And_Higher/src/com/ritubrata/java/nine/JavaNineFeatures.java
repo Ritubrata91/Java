346e4d96a88390b88a897fb607a9f48f720fba08
Ritubrata Chatterjee <ritubrata91@gmail.com>
2022-06-04 20:58:23 +0530

    example of of() and coyOf() method

--- Java_Nine_And_Higher/src/com/ritubrata/java/nine/JavaNineFeatures.java
+++ Java_Nine_And_Higher/src/com/ritubrata/java/nine/JavaNineFeatures.java
@@ -1,5 +1,30 @@
 package com.ritubrata.java.nine;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JavaNineFeatures {
 
+	public static void copyOfAndOfMethod() {
+		final List<String> list = List.copyOf(Arrays.asList("red", "green", "blue"));
+		System.out.println("After using List.copyOf method : "+list);
+		list.add("violet");// throws UnsupportedOperationException
+
+		final List<String> list_two = List.copyOf(Arrays.asList("red", "green", null)); // throws NullPointerException
+		System.out.println(list_two);
+	}
+
+	public static void ofMethodExampled() {
+		final List<String> list = List.of("red", "green", "blue");
+		System.out.println("After using List.of method : " +list);
+		list.add("violet"); // throws UnsupportedOperationException
+
+		final List<String> list_two = List.of("red", "green", null); // throws NullPointerException
+		System.out.println(list_two);
+	}
+
+
+	public static void main(final String[] args) {
+		ofMethodExampled();
+	}
 }
