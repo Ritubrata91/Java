f869a980aa85b25b61d357dcca0ff160e10108f3
Ritubrata Chatterjee <ritubrata91@gmail.com>
2022-06-12 18:19:01 +0530

    Java 14: switch expression

--- Java_Nine_And_Higher/src/com/ritubrata/java/thirteen/JavaFourteenExamples.java
+++ Java_Nine_And_Higher/src/com/ritubrata/java/thirteen/JavaFourteenExamples.java
@@ -2,9 +2,29 @@ package com.ritubrata.java.thirteen;
 
 public class JavaFourteenExamples {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static String switchExpression(final int day) {
+		final String dayOfWeek = switch (day) {
+		case 1->  {
+			System.out.println("some logic");
+			yield "Sunday";
+		}
+		case 2-> {
+			System.out.println("some logic");
+			yield  "Monday";
+		}
+		case 3->  {
+			System.out.println("some logic");
+			yield "Tuesday";
+		}
 
+		default-> throw new IllegalArgumentException("Unexpected value-> " + day);
+		};
+		return dayOfWeek;
+
+	}
+
+	public static void main(final String[] args) {
+		System.out.println(switchExpression(3));
 	}
 
 }
